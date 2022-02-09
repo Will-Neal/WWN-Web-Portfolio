9845d62d5f0c3b369d432b55a6e920b185b09aa2
Will Neal <willwneal@gmail.com>
2022-02-08 20:38:42 -0500

    Added JS to make the button stay centered when the images are hovered over

--- assets/script.js
+++ assets/script.js
@@ -1,11 +1,16 @@
 console.log("The script is linked")
 
-//grabbing the three buttons as variables
+//grabbing the three buttons and storing as variables
 let smartShopperButton = document.querySelector("#smartShopperRepo")
 let weatherButton = document.querySelector("#weatherRepo")
 let calendarButton = document.querySelector("#calendarRepo")
 
-//adding event listeners and functions
+//grabbing the 3 images and storing as variables
+let smartShopperImage = document.querySelector("#smartShopperImage")
+let weatherImage = document.querySelector("#weatherImage")
+let calendarImage = document.querySelector("#calendarImage")
+
+//adding event listeners and functions to the 3 repo buttons 
 
 smartShopperButton.addEventListener("click", function(){
     window.open("https://github.com/Will-Neal/SmartShopper-Application")
@@ -17,4 +22,31 @@ weatherButton.addEventListener("click", function(){
 
 calendarButton.addEventListener("click", function(){
     window.open("https://github.com/Will-Neal/Work-Day-Scheduler")
+})
+
+//Adding event listeners to the images to act on the buttons
+smartShopperImage.addEventListener("mouseenter", function(){
+    smartShopperButton.style.marginRight = "0px";
+})
+
+weatherImage.addEventListener("mouseenter", function(){
+    weatherButton.style.marginRight = "0px";
+})
+
+calendarImage.addEventListener("mouseenter", function() {
+    calendarButton.style.marginRight = "0px";
+})
+
+//Adding Event Listeners to restore the button location on mouseexit
+
+smartShopperImage.addEventListener("mouseleave", function(){
+    smartShopperButton.style.marginRight = "85px";
+})
+
+weatherImage.addEventListener("mouseleave", function(){
+    weatherButton.style.marginRight = "85px";
+})
+
+calendarImage.addEventListener("mouseleave", function() {
+    calendarButton.style.marginRight = "85px";
 })
