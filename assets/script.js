2ddee5f8d5cd1f61232609aa3a07c19244d53d4a
Will Neal <willwneal@gmail.com>
2022-02-08 20:12:44 -0500

    Got all buttons working and opening links in new tabs so that a viewer doesnt lose my portfolio and have to go back to see other projects

--- assets/script.js
+++ assets/script.js
@@ -1,5 +1,20 @@
 console.log("The script is linked")
 
+//grabbing the three buttons as variables
 let smartShopperButton = document.querySelector("#smartShopperRepo")
 let weatherButton = document.querySelector("#weatherRepo")
 let calendarButton = document.querySelector("#calendarRepo")
+
+//adding event listeners and functions
+
+smartShopperButton.addEventListener("click", function(){
+    window.open("https://github.com/Will-Neal/SmartShopper-Application")
+})
+
+weatherButton.addEventListener("click", function(){
+    window.open("https://github.com/Will-Neal/Realtime-Weather-App")
+})
+
+calendarButton.addEventListener("click", function(){
+    window.open("https://github.com/Will-Neal/Work-Day-Scheduler")
+})
