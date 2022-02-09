1d456c0c0a5f9e48a227e297f3ec5d4ed204f345
Will Neal <willwneal@gmail.com>
2022-02-08 20:02:11 -0500

    added JS file and linked. Got rid of deployment button because I will make the image the link

---
+++ assets/script.js
@@ -0,0 +1,5 @@
+console.log("The script is linked")
+
+let smartShopperButton = document.querySelector("#smartShopperRepo")
+let weatherButton = document.querySelector("#weatherRepo")
+let calendarButton = document.querySelector("#calendarRepo")
